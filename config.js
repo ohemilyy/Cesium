b8fe241b7aa865bfdfe525788089f44c911eadda
ItzBunniYT <55363475+ItzBunniYT@users.noreply.github.com>
2021-05-09 12:19:56 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,8 @@
 exports.token = "gay"
-exports.owners = ["491134035863666708", "621948420143316994"];
-exports.prefix = "s!";
+exports.owners = ["", ""];
 exports.sencolor = "#ff2d2d";
 exports.logo = "client.user.displayAvatarURL()";
 exports.version = "0.1";
-exports.developers = ["704255619473080332"];
+exports.developers = [""];
 exports.error = "<:error:732812802695102504> ";
 exports.yes = "<:yes:733961346370109440> ";
