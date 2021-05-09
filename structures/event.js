b308164991a2b15fce0356e79d9180a1a3091fa2
Emily Gurnee <emily@phantomhq.club>
2021-05-09 13:49:53 -0500

    Some code cleanup

--- structures/event.js
+++ structures/event.js
@@ -4,12 +4,36 @@ const filePath = join(__dirname, '..', "events");
 
 module.exports.run = (client) => {
   const eventFiles = readdirSync(filePath);
+  for (const eventFiles of readdirSync(filePath)) {
+    const event = require(`${filePath}/${eventFiles}`);
+    const eventName = eventFiles.split(".").shift();
+    client.on(eventName, event.bind(null, client));
+
+  }
+
+  console.log(`Loaded ${eventFiles.length} events! YAYA`);
+}
+
+
+//REAL ONE THAT WORKS FOR ME
+
+/*  const eventFiles = readdirSync(filePath);
   for(const eventFile of eventFiles) {
     const event = require(`${filePath}/${eventFile}`);
     const eventName = eventFile.split(".").shift();
-    client.on(eventName, event.bind(null, client));
-    
-  }
-  
-  console.log(`Loaded ${eventFiles.length} events!`);
-}
+    client.on(eventName, event.bind(null, client)); */
+// okay that works
+
+// const { readdirSync } = require("fs");
+// > const { join } = require("path");
+// > const filePath = join(__dirname, "..", "events");
+// >
+// > module.exports.run = (client) => {
+// >     const eventFiles = readdirSync(filePath);
+// >     for (const eventFiles of readdirSync(filePath)) {
+// >         const event = require(`${filePath}/${eventFiles}`);
+// >         const eventName = eventFiles.split(".").shift();
+// >         client.on(eventName, event.bind(null, client));
+// >     }
+// >
+// >     console.log(`${eventFiles.length} events chargés !`)
