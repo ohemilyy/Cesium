f1b6f5de5dca42ded415f6a6999462f4d65de9f3
Emily <55363475+ItzBunniYT@users.noreply.github.com>
2021-06-07 00:30:28 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-exports.token = "ODI4NzA0MzE3MzA0OTk1ODcz.YGtdCQ.Ag37OLjq85fpj4DqmfX8F0s4Gpw"
+exports.token = "";
 exports.owners = ["559497100296060939", "777653679506980875"]; //Owners Or Devs
 exports.prefix = ";";
